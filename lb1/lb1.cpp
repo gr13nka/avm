a5fcb66f701cf6def8ffe5a129f4b50b4db09f9b
gr13nka <gran-san2003@yandex.ru>
2023-09-14 17:59:19 +0300

    partialy working

--- lb1/lb1.cpp
+++ lb1/lb1.cpp
@@ -5,63 +5,10 @@
 #include <limits>
 #include <string.h>
 
-union {
-	int num;
-	long lnum;
-	double dnum;
-}data;
-
-int convert_to_bin(float num)
-{
-	float binary;
-	std::string len = std::to_string(num);
-	int num_len = len.length();
-	std::cout<< len<<'\n';
-
-	for(num_len; num>1 ;num_len--)
-	{
-		binary = num/(2*num_len);
-	}
-	
-	return binary;
-}
-
-int convert_num(float num)
-{
-	//for float
-	int P = 127;
-
-	//convert to bin
-	convert_to_bin(15.123456212127);
-	std::cout << num;
-
-	return 0;
-}
-
 template<typename T>
-T read_num()
+unsigned long long printBinaryNum(T num)
 {
-	T num;
-	std::cout << "Enter a number" << '\n';
-	std::cin >> num;
-	return num;
-}
-
-void printDoubleBinary(double num) {
-    unsigned long long bits = *reinterpret_cast<unsigned long long*>(&num);
-	//reinterpret_cast - change type without changing bits
-	//double is 64 bits, same as unsigned long long
-	int byte = 8;
-
-    for (int i = sizeof(double) * byte - 1; i >= 0; --i) {
-        std::cout << ((bits >> i) & 1);
-    }
-
-    std::cout << std::endl;
-}
-
-unsigned long long printLongBinary(long value) {
-    unsigned long long bits = *reinterpret_cast<unsigned long long*>(&value);
+	unsigned long long bits = *reinterpret_cast<unsigned long long*>(&num);
 	int byte = 8;
 
     for (int i = sizeof(long) * byte - 1; i >= 0; --i) {
@@ -71,34 +18,63 @@ unsigned long long printLongBinary(long value) {
     std::cout << '\n';
 	return bits;
 }
-
-void cycle_move(unsigned long long bits)
+void read_values(int& start, int& amount, int& times, int& direction)
 {
-	int i,j,n,direction;
 	std::cout << "Enter from which bit to move"<< '\n';
-	std::cin >> i;
-	std::cout << "Enter until which bit to move"<< '\n';
-	std::cin >> j;
+	std::cin >> start;
+	std::cout << "Enter amount of bit to move"<< '\n';
+	std::cin >> amount;
 	std::cout << "Enter in which direction to move, 1 for right and 0 for left"<< '\n';
 	std::cin >> direction;
 	std::cout << "Enter how many times to move"<< '\n';
-	std::cin >> n;
+	std::cin >> times;
 
-	int byte = 8;
-	unsigned long long changing_bits;
-	unsigned long long a[sizeof(bits)];
+	times = times % amount;
+}
 
-	//append part of bits to an array
-	for (i;i<j;i++)
+unsigned long long cycle_move(unsigned long long bits)
+{
+	int start, amount, times, direction;
+//	read_values(start, amount, times, direction);
+	start = direction = 0;
+	amount = 3;
+	times = 1;
+
+	unsigned long long Max = 922337203685477600;
+	int bittomove = 0;
+
+	if(times == amount)
+		return bits;
+
+	int changed_bits[64] = {0};
+	
+	//filling array
+	for(int i = 64; i>=0; --i)
 	{
-		a[i] = (bits >> j*byte);
-		std::cout<< a[i];
+		changed_bits[i] = (bits>>i)&1;
+		std::cout << changed_bits[i];
+	}
+	std::cout << '\n';
+
+	int last;
+	for(int i = 0; i<=times; i++)
+	{
+		last = changed_bits[amount];
+
+		for(int j = start+1; j<amount-1; j++)
+		{
+			changed_bits[j] = changed_bits[j+1];
+		}
+
+		changed_bits[start] = last;
+	}
+	
+	for(int i = 64; i>=0; --i)
+	{
+		std::cout << changed_bits[i];
 	}
 
-	//cycle the array
-	
-	
-
+return  bits;
 }
 
 int main() {
@@ -110,20 +86,20 @@ int main() {
 	//same size 
 	std::cout <<"long " << sizeof(long);
 	std::cout <<"double " << sizeof(double);
-	std::cout <<"unsigned long long" << sizeof(unsigned long long) << '\n';
 
-	std::cout << "Enter double "<< '\n';
-	std::cin >> d;
+	std::cout <<"unsigned long long" << sizeof(unsigned long long) << '\n';
+	//std::cin >> d;
 
 	std::cout << "Enter long "<< '\n';
-	std::cin >> l;
+//	std::cin >> l;
 
-    //std::cout << "Binary representation of double: ";
-   // d_result = printDoubleBinary(d);
+    std::cout << "Binary representation of double: ";
+    d_result = printBinaryNum(d);
 
     std::cout << "Binary representation of long: ";
-    l_result = printLongBinary(l);
+    l_result = printBinaryNum(l);
 
-
+	cycle_move(l_result);
+		
     return 0;
 }
